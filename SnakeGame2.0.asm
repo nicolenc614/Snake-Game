f22f4b7865475a4814d3fe3f07ff22232cac5166
nicole chavarria <71100444+nicolenc614@users.noreply.github.com>
2021-03-11 17:50:20 -0500

    Update SnakeGame2.0.asm

--- SnakeGame2.0.asm
+++ SnakeGame2.0.asm
@@ -90,7 +90,25 @@ CALL startGame ; Call main infinite loop
 main ENDP
 
 initializeSnake PROC
-
+; This procedure initializes the snake to the default position
+; in the center of the screen
+MOV DH, 13 ; Set row number to 13
+MOV DL, 47 ; Set column number to 47
+MOV BX, 1 ; First segment of snake
+CALL saveIndex ; Write to framebuffer
+MOV DH, 14 ; Set row number to 14
+MOV DL, 47 ; Set column number to 47
+MOV BX, 2 ; Second segment of snake
+CALL saveIndex ; Write to framebuffer
+MOV DH, 15 ; Set row number to 15
+MOV DL, 47 ; Set column number to 47
+MOV BX, 3 ; Third segment of snake
+CALL saveIndex ; Write to framebuffer
+MOV DH, 16 ; Set row number to 16
+MOV DL, 47 ; Set column number to 47
+MOV BX, 4 ; Fourth segment of snake
+CALL saveIndex ; Write to framebuffer
+RET
 initializeSnake ENDP
 
 Paint PROC
